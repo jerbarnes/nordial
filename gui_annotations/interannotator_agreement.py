c8c6c0c5664effcf58d25768f5f626b981ba88d5
jeremy <jbarnesspain@gmail.com>
2021-09-08 12:24:41 +0200

    added argparse to code: now you can print disagreements with --verbose flag

--- gui_annotations/interannotator_agreement.py
+++ gui_annotations/interannotator_agreement.py
@@ -1,6 +1,7 @@
 import json
 import krippendorff
 import sys
+import argparse
 
 def all_same(items):
     return all(x == items[0] for x in items)
@@ -8,10 +9,15 @@ def all_same(items):
 label_dict = {"bokmål": 0, "nynorsk": 1, "dialectal": 2, "mixed": 3}
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("annotation_files", nargs="+")
+    parser.add_argument("--verbose", action="store_true")
+
+    args = parser.parse_args()
     all_data = []
     all_idxs = []
     disagreements = []
-    for file in sys.argv[1:]:
+    for file in args.annotation_files:
         print(file)
         with open(file) as o:
             d = json.load(o)
@@ -32,20 +38,21 @@ if __name__ == "__main__":
     alpha = krippendorff.alpha(all_data, level_of_measurement="nominal")
     print("Kripp Alpha: {0:.3f}".format(alpha))
 
-    for d1, idx1, file1 in zip(all_data, all_idxs, sys.argv[1:]):
-        for d2, file2 in zip(all_data, sys.argv[1:]):
-            if d1 is not d2:
-                print("Disagreements between {} and {}".format(file1, file2))
-                disagree_idxs = []
-                for i in range(len(d1)):
-                    if d1[i] != d2[i]:
-                        disagree_idxs.append(idx1[i])
-                with open(file1) as o:
-                    lfile1 = json.load(o)
-                with open(file2) as o:
-                    lfile2 = json.load(o)
-                for idx in disagree_idxs:
-                    print(lfile1[str(idx)])
-                    print(lfile2[str(idx)])
-                    print("-" * 40)
-                print("=" * 80)
+    if args.verbose:
+        for d1, idx1, file1 in zip(all_data, all_idxs, sys.argv[1:]):
+            for d2, file2 in zip(all_data, sys.argv[1:]):
+                if d1 is not d2:
+                    print("Disagreements between {} and {}".format(file1, file2))
+                    disagree_idxs = []
+                    for i in range(len(d1)):
+                        if d1[i] != d2[i]:
+                            disagree_idxs.append(idx1[i])
+                    with open(file1) as o:
+                        lfile1 = json.load(o)
+                    with open(file2) as o:
+                        lfile2 = json.load(o)
+                    for idx in disagree_idxs:
+                        print(lfile1[str(idx)])
+                        print(lfile2[str(idx)])
+                        print("-" * 40)
+                    print("=" * 80)
